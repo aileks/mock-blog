ed33e75d181b5018c3874af78c01f07f4f559776
aileks <88669313+aileks@users.noreply.github.com>
2023-10-31 13:12:58 -0400

    Eloquent models

---
+++ resources/views/post.blade.php
@@ -0,0 +1,10 @@
+<x-layout>
+    <article>
+        <h1>{{ $post->title }}</h1>
+        <div>
+            {!! $post->body !!}
+        </div>
+    </article>
+
+    <a href="/">Go Back</a>
+</x-layout>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,15 @@
+<x-layout>
+    @foreach ($posts as $post)
+        <article>
+            <h1>
+                <a href="/posts/{{ $post->id }}">
+                    {{ $post->title }}
+                </a>
+            </h1>
+
+            <div>
+                {{ $post->excerpt }}
+            </div>
+        </article>
+    @endforeach
+</x-layout>
