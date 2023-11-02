39540c1509c952d47fcd64654e13d891a0328c23
aileks <88669313+aileks@users.noreply.github.com>
2023-11-02 14:38:59 -0400

    Factories

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,5 +9,20 @@ class Post extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'excerpt', 'body'];
+    protected $guarded = [];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- database/migrations/2023_10_31_150810_create_posts_table.php
+++ database/migrations/2023_10_31_150810_create_posts_table.php
@@ -12,6 +12,9 @@ return new class extends Migration {
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('category_id');
+            $table->foreignId('user_id');
+            $table->string('slug')->unique();
             $table->string('title');
             $table->text('excerpt');
             $table->text('body');
