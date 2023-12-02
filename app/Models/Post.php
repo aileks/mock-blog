2232a227d65aacdc5f065a1fe856102aab5b88e8
aileks <88669313+aileks@users.noreply.github.com>
2023-12-02 13:43:42 -0500

    I do not remember what I did :D

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Post extends Model
 {
@@ -36,6 +37,11 @@ class Post extends Model
             $query->where('username', $author)
             )
         );
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
     }
 
     public function category(): BelongsTo
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -48,6 +48,13 @@
                         {!! $post->body !!}
                     </div>
                 </div>
+
+                <section class="col-span-8 col-start-5 mt-10 space-y-6">
+                    @foreach ($post->comments as $comment)
+                        <x-post-comment :comment="$comment"/>
+                    @endforeach
+                </section>
+
             </article>
         </main>
     </section>
