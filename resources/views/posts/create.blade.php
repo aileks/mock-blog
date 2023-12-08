d4ba48c6690db9d441f6e06ab249c8f65276b1c5
aileks <88669313+aileks@users.noreply.github.com>
2023-12-08 17:43:55 -0500

    Made thumbnails dynamic.

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,9 +1,12 @@
 @php use App\Models\Category; @endphp
 
 <x-layout>
-    <section class="px-6 py-8">
-        <x-panel class="max-w-sm mx-auto">
-            <form method="POST" action="/admin/posts">
+    <section class="py-8 max-w-md mx-auto">
+        <h1 class="text-lg font-bold mb-4">
+            Publish New Post
+        </h1>
+        <x-panel>
+            <form method="POST" action="/admin/posts" enctype="multipart/form-data">
                 @csrf
 
                 <div class="mb-6">
@@ -42,6 +45,22 @@
                     @enderror
                 </div>
 
+                <div class="mb-6">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                           for="thumbnail">
+                        Thumbnail
+                    </label>
+
+                    <input class="border border-gray-400 p-2 w-full"
+                           type="file"
+                           name="thumbnail"
+                           id="thumbnail"
+                           required>
+
+                    @error('thumbnail')
+                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                    @enderror
+                </div>
 
                 <div class="mb-6">
                     <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
